61fa559f1462eb0965f099c7b9f8f7e21e16b76c
Szymon Pajzert <szymonpajzert@gmail.com>
2025-06-20 14:46:19 +0200

    files added now

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('home', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    cy.get('.v-card').should('have.length', 3)
+    cy.get('img')
+      .should('be.visible')
+      .and('have.prop', 'naturalWidth')
+      .should('be.greaterThan', 0)
+  })
+
+  it('displays three clickable cards', () => {
+    cy.get('.v-card').first().contains('Zobacz listę')
+    cy.get('.v-card').eq(1).contains('Dodaj osoby')
+    cy.get('.v-card').eq(2).contains('Dołącz do projektu')
+    // See https://github.com/jaredpalmer/cypress-image-snapshot?tab=readme-ov-file#usage
+    // E.g. --env updateSnapshots=true
+    cy.get("body").matchImageSnapshot()
+  })
+})
