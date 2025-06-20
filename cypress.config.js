f5770359efdf3ad9cd2bd1c2a643720427ba6815
Szymon Pajzert <szymonpajzert@gmail.com>
2025-06-20 15:04:56 +0200

    add cypress

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "cypress";
+import { addMatchImageSnapshotPlugin } from '@simonsmith/cypress-image-snapshot/plugin'
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on) {
+      addMatchImageSnapshotPlugin(on)
+    },
+    baseUrl: 'http://localhost:5002',
+  },
+});
---
+++ snapshots.js
@@ -0,0 +1,3 @@
+module.exports = {
+  "__version": "14.5.0"
+}
