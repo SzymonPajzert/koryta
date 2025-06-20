364b7e6d581ffc407cc10e66e921d431888a8f99
Szymon Pajzert <szymonpajzert@gmail.com>
2025-06-20 15:01:17 +0200

    home page is shown with loaded data

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -3,14 +3,16 @@
 describe('home', () => {
   beforeEach(() => {
     cy.visit('/')
+  })
+
+  it('displays three clickable cards', () => {
     cy.get('.v-card').should('have.length', 3)
     cy.get('img')
       .should('be.visible')
       .and('have.prop', 'naturalWidth')
       .should('be.greaterThan', 0)
-  })
+    cy.get('.vue-apexcharts').matchImageSnapshot('loaded-graph', { padding: 50 })
 
-  it('displays three clickable cards', () => {
     cy.get('.v-card').first().contains('Zobacz listę')
     cy.get('.v-card').eq(1).contains('Dodaj osoby')
     cy.get('.v-card').eq(2).contains('Dołącz do projektu')
