d88caf003907781c4d2759113f19f63bfa6fd3a9
Szymon Pajzert <szymonpajzert@gmail.com>
2025-07-12 19:43:20 +0200

    remove Home component and move it all to index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,42 @@
 <template>
-  <Home />
+  <div>
+    <v-row>
+      <v-col cols="8">
+        <v-img class="mb-4" height="150" src="@/assets/świnia.png" />
+
+        <div class="mb-8 text-center">
+          <h1 class="text-h2 font-weight-bold">koryta.pl</h1>
+          <div class="text-body-2 font-weight-light mb-n1">
+            {{ subtitle }}
+          </div>
+        </div>
+      </v-col>
+      <v-col cols="4">
+        <PartyChart />
+      </v-col>
+
+      <HomeItem router="list" icon="mdi-text-box-outline">
+        <template #header>
+          Zobacz listę {{ Object.values(people).length }} {{ koryciarz.plural.genitive }}
+        </template>
+        Osób na ciepłych państwowych posadkach, dzięki ich rodzicom, rodzeństwu lub kolegom z pracy
+      </HomeItem>
+      <HomeItem router="pomoc" icon="mdi-plus-box-outline">
+        <template #header> Dodaj osoby i artykuły </template>
+        Dodaj brakujące osoby w spółkach państwa lub samorządu albo linki do artykułów wypisujących je
+      </HomeItem>
+    </v-row>
+  </div>
 </template>
 
-<script lang="ts" setup>
-  import Home from '@/components/Home.vue';
+<script setup lang="ts">
+import { useFeminatyw } from "@/composables/feminatyw";
+import { useListEntity } from "@/composables/entity";
+import { isTest } from "@/firebase";
+
+const { entities: people } = useListEntity("employed");
+const { koryciarz } = useFeminatyw();
+
+const idx = isTest() ? 1 : Math.floor(Math.random() * 2)
+const subtitle = ["Polityczny wypas", "Pójdź tam, gdzie politycy zimują"][idx]
 </script>
