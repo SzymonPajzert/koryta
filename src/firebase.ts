b4df62527be4e885ba2ecb4ef55303349a2cf9be
Filip Wachowiak <wachowiakf@gmail.com>
2025-07-15 18:42:24 +0200

    fix build

--- src/firebase.ts
+++ src/firebase.ts
@@ -30,9 +30,6 @@ export const analytics = getAnalytics(app);
 export const db = getDatabase(app);
 export const auth = getAuth(app);
 export const functions = getFunctions(app, "europe-west1");
-export function isTest() {
-  return location.hostname === "localhost" || location.hostname == "127.0.0.1";
-}
 
 if (isTest()) {
   connectDatabaseEmulator(db, "127.0.0.1", 9003);
