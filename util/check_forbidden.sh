4bcea015ab612a04db9d4d17cd8e0de096160b4e
Szymon Pajzert <szymonpajzert@gmail.com>
2025-09-03 15:17:55 +0200

    Modify checking for TODOs (#64)

--- util/check_forbidden.sh
+++ util/check_forbidden.sh
@@ -6,6 +6,8 @@
 
 grep -r -n \
     -E 'TODO|console.log' \
-    --exclude-dir=node_modules --exclude-dir=.git --exclude-dir=.jj --exclude-dir=dist \
+    --exclude-dir=node_modules --exclude-dir=.git --exclude-dir=.jj \
+    --exclude-dir=dist --exclude-dir=.nuxt --exclude-dir=.output \
+    --exclude-dir=wiki --exclude-dir=venv12 --exclude-dir=.venv \
     --exclude=check_forbidden.sh \
     . | grep -vE 'TODO\(https://github.com/SzymonPajzert/koryta/issues/[0-9]+\)'
