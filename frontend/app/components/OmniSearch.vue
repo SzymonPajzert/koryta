2708e85061c7b63d239e2881da17a063c943be04
Szymon Pajzert <szymonpajzert@gmail.com>
2025-09-04 00:57:30 +0200

    OmniSearch lists people

--- frontend/app/components/OmniSearch.vue
+++ frontend/app/components/OmniSearch.vue
@@ -10,9 +10,10 @@
   <v-autocomplete
     v-model="nodeGroupPicked"
     v-model:focused="autocompleteFocus"
+    v-model:search="search"
     label="Szukaj osoby albo miejsca"
     :items="items"
-    item-title="name"
+    item-title="title"
     return-object
     autocomplete="off"
     class="ma-2"
@@ -24,22 +25,38 @@
     menu-icon="mdi-magnify"
     clearable
     single-line
+    @click:clear="nodeGroupPicked = null"
   >
     <template #item="{ props, item }">
       <v-list-item
         v-bind="props"
-        :subtitle="`${item.raw.stats.people} powiązanych osób`"
-        :title="item.raw.name"
+        :subtitle="item.raw?.subtitle"
+        :title="item.raw.title"
         max-width="400px"
         :prepend-icon="item.raw.icon"
       />
+    </template>
+    <template #no-data>
+      <v-list-item v-if="!search">
+        <v-list-item-title> Ładuję dane... </v-list-item-title>
+      </v-list-item>
     </template>
   </v-autocomplete>
 </template>
 
 <script setup lang="ts">
-import type { NodeGroup } from "~~/shared/graph/model";
 import { getAnalytics, logEvent } from "firebase/analytics";
+
+const search = ref("");
+
+type ListItem = {
+  title: string;
+  subtitle?: string;
+  icon: string;
+  logEventKey: {content_id: string; content_type: string};
+  path?: string;
+  query?: Record<string, string>;
+};
 
 const { data: graph } = await useAsyncData(
   "graph",
@@ -47,36 +64,68 @@ const { data: graph } = await useAsyncData(
   { lazy: true },
 );
 
-const items = computed(() => {
+const items = computed<ListItem[]>(() => {
   if (!graph.value) return [];
-  const result = [];
+  const result: ListItem[] = [];
   result.push({
-    ...graph.value.nodeGroups[0],
-    name: "Lista wszystkich osób",
+    title: "Lista wszystkich osób",
+    subtitle: `${graph.value.nodeGroups[0].stats.people} powiązanych osób`,
     icon: "mdi-format-list-bulleted-type",
     path: "/lista",
+    logEventKey: {
+      content_id: graph.value.nodeGroups[0].id,
+      content_type: "nodeGroup",
+    },
   });
   result.push({
-    ...graph.value.nodeGroups[0],
-    name: "Graf wszystkich osób",
+    title: "Graf wszystkich osób",
+    subtitle: `${graph.value.nodeGroups[0].stats.people} powiązanych osób`,
     icon: "mdi-graph-outline",
     path: "/graf",
+    logEventKey: {
+      content_id: graph.value.nodeGroups[0].id,
+      content_type: "nodeGroup",
+    },
   });
-  graph.value.nodeGroups
-    .slice(1)
-    .forEach((item) => result.push({ ...item, icon: "mdi-domain" }));
+  graph.value.nodeGroups.slice(1).forEach((item) =>
+    result.push({
+      title: item.name,
+      subtitle: `${item.stats.people} powiązanych osób`,
+      icon: "mdi-domain",
+      logEventKey: {
+        content_id: item.id,
+        content_type: "nodeGroup",
+      },
+      query: {
+        miejsce: item.id,
+      },
+    }),
+  );
+  Object.entries(graph.value.nodes).forEach(([key, value]) => {
+    if (value.type == "circle") {
+      result.push({
+        title: value.name,
+        icon: "mdi-account",
+        logEventKey: {
+          content_id: key,
+          content_type: "person",
+        },
+        path: "/entity/employed/" + key,
+      });
+    }
+  });
   return result;
 });
 
 const { push, currentRoute } = useRouter();
 const analytics = getAnalytics();
 
-const nodeGroupPicked = ref<NodeGroup & { path?: string }>();
+const nodeGroupPicked = ref<ListItem | null>();
 const autocompleteFocus = ref(false);
 watch(nodeGroupPicked, (value) => {
-  if (!value) return;
+  if (!value) push("/");
   let path = value.path ?? currentRoute.value.path;
-  const allowedPath = path == "/lista" || path == "/graf";
+  const allowedPath = path == "/lista" || path == "/graf" || path.startsWith("/entity/employed/");
   if (!allowedPath) {
     path = "/lista";
   }
@@ -84,13 +133,10 @@ watch(nodeGroupPicked, (value) => {
     path: path,
     query: {
       ...currentRoute.value.query,
-      miejsce: value.id,
+      ...value.query,
     },
   });
-  logEvent(analytics, "select_content", {
-    content_id: value.id,
-    content_type: "nodeGroup",
-  });
+  logEvent(analytics, "select_content", value.logEventKey);
   autocompleteFocus.value = false;
 });
 </script>
