d21144f4be6139cf71f4971ff03fbe3ce2878137
Szymon Pajzert <szymonpajzert@gmail.com>
2025-09-15 13:33:37 +0200

    Run local emulators and tests
    Configure emulators to work with nuxt
    Add vitest and fix emulators

--- database/import_firestore.sh
+++ database/import_firestore.sh
@@ -5,15 +5,16 @@ set -e
 # Initiate a new export to a koryta-pl-firestore-backups Cloud Storage
 
 # Set the timestamp of the export here
-LATEST_TIMESTAMP=2025-09-13T12:13:52_72256
+LATEST_TIMESTAMP=2025-09-15T17:04:37_69979
 
 if ! [[ -d ./${LATEST_TIMESTAMP} ]] ; then
     echo "Downloading the latest export"
     gsutil -m cp -r gs://koryta-pl-firestore-backups/${LATEST_TIMESTAMP} .
 fi
 
-firebase emulators:start --import=./${LATEST_TIMESTAMP}
+cp -r ${LATEST_TIMESTAMP} current_firestore
 
+# firebase emulators:start --import=./${LATEST_TIMESTAMP}
 # You should be able to see the DB in http://127.0.0.1:4000/firestore/default/data
 # Remember to set `export FIRESTORE_EMULATOR_HOST="127.0.0.1:8080"` to access it from scripts.
 # You can dump the modified data with `firebase emulators:export ./firestore_modified`
