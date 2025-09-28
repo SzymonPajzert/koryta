a541084e40a2a7281c5d0e62374d8ac21bdbd9bf
Szymon Pajzert <szymonpajzert@gmail.com>
2025-09-28 22:17:36 +0200

    Download and process KRS data
    Create a jupyter notebook for better analysis
    
    Check the metaphone for outer matches - more people listed - currently 3k
    
    Analyze people
    
    Scrape children from KRS
    
    Spółki skarbu państwa
    
    Show matches across the whole group
    
    Remove modularity of the code
    
    Download from firestore to jsonl

--- data/scrapers/src/util/rejestr.py
+++ data/scrapers/src/util/rejestr.py
@@ -3,35 +3,37 @@ import sys
 from dotenv import load_dotenv
 import requests
 
-load_dotenv()
-REJESTR_KEY = os.getenv("REJESTR_KEY")
-print("Reading rejestr.io key: ", REJESTR_KEY)
 
-if not REJESTR_KEY:
-    print("Not found, go to https://rejestr.io/konto/api and set it in .env")
-    sys.exit(1)
+class Rejestr:
+    def __init__(self):
+        load_dotenv()
+        self.REJESTR_KEY = os.getenv("REJESTR_KEY")
+        print("Reading rejestr.io key: ", self.REJESTR_KEY)
 
-ALWAYS_ALLOW = False
-print("Should I always ask before querying rejestr.io? [Yn]")
-if input() == "n":
-    print("I will always allow")
-    ALWAYS_ALLOW = True
-else:
-    print("I will always ask for permission")
+        if not self.REJESTR_KEY:
+            print("Not found, go to https://rejestr.io/konto/api and set it in .env")
+            sys.exit(1)
 
+        self.ALWAYS_ALLOW = False
+        print("Should I always ask before querying rejestr.io? [Yn]")
+        if input() == "n":
+            print("I will always allow")
+            self.ALWAYS_ALLOW = True
+        else:
+            print("I will always ask for permission")
 
-def get_rejestr_io(url: str):
-    allowed = ALWAYS_ALLOW
-    print(f"Querying {url}")
-    if not ALWAYS_ALLOW:
-        print(f"Should I query? [yN]")
-        allowed = input() == "y"
-    if not allowed:
-        print("Not allowed")
-        return {}
+    def get_rejestr_io(self, url: str):
+        allowed = self.ALWAYS_ALLOW
+        print(f"Querying {url}")
+        if not self.ALWAYS_ALLOW:
+            print(f"Should I query? [yN]")
+            allowed = input() == "y"
+        if not allowed:
+            print("Not allowed")
+            return {}
 
-    response = requests.get(url, headers={"Authorization": REJESTR_KEY})
-    if response.status_code != 200:
-        print(response.status_code)
-        return None
-    return response.text
+        response = requests.get(url, headers={"Authorization": self.REJESTR_KEY})
+        if response.status_code != 200:
+            print(response.status_code)
+            return None
+        return response.text
