7b291a5f1cefb8aeeb37ca34fa58c1c0f017a28e
Szymon Pajzert <szymonpajzert@gmail.com>
2025-10-07 14:27:17 +0200

    Download and process KRS data (#106)
    Scrape children from KRS
    Spółki skarbu państwa
    Download from firestore to jsonl

--- data/scrapers/src/scrapers/krs/process.py
+++ data/scrapers/src/scrapers/krs/process.py
@@ -6,6 +6,29 @@ from stores.storage import list_blobs, download_from_gcs
 from stores.duckdb import ducktable, dump_dbs
 
 
+class KRS:
+    id: str
+
+    def __init__(self, id: int | str) -> None:
+        self.id = str(id).zfill(10)
+
+    @staticmethod
+    def from_blob_name(blob_name: str) -> "KRS":
+        return KRS(blob_name.split("org/")[1].split("/")[0])
+
+    def __str__(self) -> str:
+        return self.id
+
+    def __repr__(self) -> str:
+        return self.id
+
+    def __hash__(self) -> int:
+        return hash(self.id)
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, KRS) and self.id == other.id
+
+
 @ducktable(name="people_krs")
 @dataclass
 class KrsPerson:
@@ -13,6 +36,7 @@ class KrsPerson:
     first_name: str
     last_name: str
     full_name: str
+    krs: str
     birth_date: str | None = None
     second_names: str | None = None
     sex: str | None = None
@@ -24,24 +48,30 @@ class KrsPerson:
         pass
 
 
+def iterate_blobs():
+    blobs = list_blobs("rejestr.io")
+    for blob_name in tqdm(blobs):
+        if "krs-powiazania" not in blob_name:
+            continue
+        content = download_from_gcs(blob_name)
+        if not content:
+            print(f"  [ERROR] Could not download {blob_name}")
+            continue
+        try:
+            data = json.loads(content)
+            yield blob_name, data
+        except json.JSONDecodeError as e:
+            print(f"  [ERROR] Could not process {blob_name}: {e}")
+
+
 def extract_people():
     """
     Iterates through GCS files from rejestr.io, parses them,
     and extracts information about people.
     """
-    blobs = list_blobs("rejestr.io")
     try:
-        for blob_name in tqdm(blobs):
-            if "krs-powiazania" not in blob_name:
-                print(f"Skipping")
-                continue
-            print(f"Processing {blob_name}")
-            content = download_from_gcs(blob_name)
-            if not content:
-                print(f"  [ERROR] Could not download {blob_name}")
-                continue
+        for blob_name, data in iterate_blobs():
             try:
-                data = json.loads(content)
                 for item in data:
                     if item.get("typ") == "osoba":
                         identity = item.get("tozsamosc", {})
@@ -53,8 +83,9 @@ def extract_people():
                             birth_date=identity.get("data_urodzenia"),
                             second_names=identity.get("drugie_imiona"),
                             sex=identity.get("plec"),
+                            krs=KRS.from_blob_name(blob_name).id,
                         ).insert_into()
-            except (json.JSONDecodeError, KeyError) as e:
+            except KeyError as e:
                 print(f"  [ERROR] Could not process {blob_name}: {e}")
     finally:
         dump_dbs()
--- data/scrapers/src/scrapers/krs/scrape.py
+++ data/scrapers/src/scrapers/krs/scrape.py
@@ -1,206 +1,177 @@
 import typing
 import itertools
+from time import sleep
+from dataclasses import dataclass
+from pprint import pprint
+from collections import Counter
+import argparse
 
-from util.rejestr import get_rejestr_io
+from scrapers.krs import data
+from scrapers.krs.process import iterate_blobs, KRS
+from util.rejestr import Rejestr
 from stores.storage import upload_to_gcs, list_blobs
 
 
-class KRS:
-    id: str
-
-    def __init__(self, id: int | str) -> None:
-        self.id = str(id).zfill(10)
-
-    def __str__(self) -> str:
-        return self.id
-
-    def __repr__(self) -> str:
-        return self.id
-
-    def __hash__(self) -> int:
-        return hash(self.id)
-
-    def __eq__(self, other: object) -> bool:
-        return isinstance(other, KRS) and self.id == other.id
+@dataclass(frozen=True)
+class Company:
+    krs: KRS
+    name: str
+    parent: KRS
 
 
 def save_org_connections(krss: typing.Iterable[KRS]):
     for krs in krss:
-        yield f"https://rejestr.io/api/v2/org/{krs}"
+        # Remember to increase the estimated price below when uncommenting
+        # yield f"https://rejestr.io/api/v2/org/{krs}"
         yield f"https://rejestr.io/api/v2/org/{krs}/krs-powiazania?aktualnosc=aktualne"
         yield f"https://rejestr.io/api/v2/org/{krs}/krs-powiazania?aktualnosc=historyczne"
 
 
-# https://www.gov.pl/attachment/55892deb-efa3-44ba-87ed-7653034ee00f from
-# https://www.gov.pl/web/aktywa-panstwowe/spolki-objete-nadzorem-wlascicielskim-ministra-aktywow-panstwowych
-MINISTERSTWO_AKTYWOW_PANSTWOWYCH_KRSs = [
-    "0000002710",
-    "0000004324",
-    "0000006791",
-    "0000007411",
-    "0000009831",
-    "0000010696",
-    "0000011737",
-    "0000012483",
-    "0000015501",
-    "0000016065",
-    "0000020227",
-    "0000022693",
-    "0000023302",
-    "0000025769",
-    "0000026438",
-    "0000027151",
-    "0000027471",
-    "0000027497",
-    "0000027591",
-    "0000028860",
-    "0000035770",
-    "0000037957",
-    "0000044577",
-    "0000045833",
-    "0000047030",
-    "0000047774",
-    "0000047934",
-    "0000049056",
-    "0000051042",
-    "0000054811",
-    "0000056031",
-    "0000056440",
-    "0000056561",
-    "0000056964",
-    "0000058915",
-    "0000059307",
-    "0000059625",
-    "0000064518",
-    "0000066285",
-    "0000067459",
-    "0000068470",
-    "0000070348",
-    "0000072093",
-    "0000073983",
-    "0000075450",
-    "0000076693",
-    "0000077664",
-    "0000081582",
-    "0000082312",
-    "0000083121",
-    "0000084678",
-    "0000086641",
-    "0000088012",
-    "0000095342",
-    "0000097998",
-    "0000099256",
-    "0000100887",
-    "0000103624",
-    "0000104717",
-    "0000108417",
-    "0000114136",
-    "0000114383",
-    "0000116560",
-    "0000132241",
-    "0000144813",
-    "0000144861",
-    "0000154178",
-    "0000162581",
-    "0000166294",
-    "0000171101",
-    "0000173377",
-    "0000185947",
-    "0000190790",
-    "0000219832",
-    "0000223325",
-    "0000228587",
-    "0000245260",
-    "0000271562",
-    "0000278401",
-    "0000287031",
-    "0000288091",
-    "0000292313",
-    "0000293205",
-    "0000294679",
-    "0000295452",
-    "0000302817",
-    "0000305180",
-    "0000308183",
-    "0000313140",
-    "0000325752",
-    "0000326587",
-    "0000334972",
-    "0000360701",
-    "0000374289",
-    "0000383595",
-    "0000384496",
-    "0000445684",
-    "0000466256",
-    "0000478929",
-    "0000489456",
-    "0000491363",
-    "0000495369",
-    "0000602576",
-    "0000638779",
-    "0000651242",
-    "0000673893",
-    "0000684374",
-    "0000709363",
-    "0000924719",
-    "0000955885",
-]
+# If relation is passive and one of this type, it's a child.
+PARENT_RELATION = {
+    "KRS_ONLY_SHAREHOLDER",
+    "KRS_SHAREHOLDER",
+    "KRS_SUPERVISION",
+    "KRS_FOUNDER",
+}
 
-# TODO What is a good source of them?
-# Dumped some manually from https://bip.warszawa.pl/jednostki-organizacyjne
-# https://docs.google.com/spreadsheets/d/14SM3cuO0gZ897T2mT40Pm4mUqXtY5JCvaemHMvs9AcQ
-WARSZAWA = [
-    "0000019230",
-    "0000030682",
-    "0000036568",
-    "0000050531",
-    "0000052192",
-    "0000085927",
-    "0000096436",
-    "0000114280",
-    "0000124188",
-    "0000145910",
-    "0000146121",
-    "0000146122",
-    "0000146125",
-    "0000146138",
-    "0000146143",
-    "0000171197",
-    "0000173077",
-    "0000206762",
-    "0000253038",
-    "0000278968",
-    "0000368174",
-    "0000380895",
-    "0000445779",
-    "0000456064",
-    "0000468274",
-    "0000478458",
-    "0000582513",
-    "0000678693",
-    "0001004272",
-    "0001009228",
-]
+# If relation is passive and one of this type, it's not a child.
+IGNORED_PARENT = {
+    "KRS_BOARD",  # The company itself is the board member
+    "KRS_MEMBER",  # The company is a member of a group, not interesting
+    "KRS_COMMISSIONER",  # The company is probably liquidated
+    "KRS_RECEIVER",  # The company is probably liquidated
+    "KRS_GENERAL_PARTNER",  # The company is probably liquidated
+    "KRS_RESTRUCTURIZATOR",  # The company is probably liquidated
+}
+
+
+@dataclass(frozen=True)
+class QueryRelation:
+    relation: str
+    direction: str
+
+    @staticmethod
+    def from_rejestrio(dict):
+        return QueryRelation(
+            relation=dict["typ"],
+            direction=dict["kierunek"],
+        )
+
+    def is_child(self):
+        if self.direction == "AKTYWNY":
+            return False
+
+        if self.relation in IGNORED_PARENT:
+            return False
+
+        if self.relation not in PARENT_RELATION:
+            raise ValueError(f"Unknown type: {self.relation} {self.direction}")
+
+        return self.direction == "PASYWNY" and self.relation in PARENT_RELATION
+
+
+class CompanyGraph:
+    def __init__(self):
+        self.companies = dict()
+        self.children: dict[KRS, list[KRS]] = dict()
+
+        for blob_name, data in iterate_blobs():
+            for item in data:
+                if item.get("typ") != "organizacja":
+                    continue
+
+                krs = KRS(item["numery"]["krs"])
+                parent = KRS.from_blob_name(blob_name)
+                conn_type = QueryRelation.from_rejestrio(
+                    item["krs_powiazania_kwerendowane"][0]
+                )
+
+                if not conn_type.is_child():
+                    continue
+
+                self.companies[krs] = Company(
+                    krs=krs,
+                    name=item["nazwy"]["pelna"],
+                    parent=parent,
+                )
+                self.children[parent] = self.children.get(parent, []) + [krs]
+
+    def all_descendants(self, krss: typing.Iterable[KRS]):
+        descendants: set[KRS] = set()
+        todo = set(krss)
+        while todo:
+            krs = todo.pop()
+            descendants.add(krs)
+            if krs in self.children:
+                todo.update(set(self.children[krs]) - descendants)
+        return descendants
+
+
+def child_companies() -> set[Company]:
+    result: set[Company] = set()
+    for blob_name, data in iterate_blobs():
+        try:
+            for item in data:
+                if item.get("typ") == "organizacja":
+                    result.add(
+                        Company(
+                            krs=KRS(item["numery"]["krs"]),
+                            name=item["nazwy"]["pelna"],
+                            parent=KRS.from_blob_name(blob_name),
+                        )
+                    )
+        except KeyError as e:
+            print(f"  [ERROR] Could not process {blob_name}: {e}")
+    return result
 
 
 def scrape_rejestrio():
-    # TODO Find children of the mentioned companies
-
-    already_scraped = set(
-        KRS(path.split("org/")[1].split("/")[0]) for path in list_blobs("rejestr.io")
+    parser = argparse.ArgumentParser(description="Say hi.")
+    parser.add_argument(
+        "--only",
+        dest="only",
+        default="",
+        help="only show children of this KRS",
     )
+    args = parser.parse_args()
+
+    already_scraped = set(KRS.from_blob_name(path) for path in list_blobs("rejestr.io"))
     starters = set(
         KRS(krs)
-        for krs in itertools.chain(MINISTERSTWO_AKTYWOW_PANSTWOWYCH_KRSs, WARSZAWA)
+        for krs in itertools.chain(
+            data.MINISTERSTWO_AKTYWOW_PANSTWOWYCH_KRSs,
+            data.WARSZAWA,
+            data.SPOLKI_SKARBU_PANSTWA,
+            data.AMW,
+            data.KRAKOW,
+        )
     )
-    to_scrape = starters - already_scraped
+    graph = CompanyGraph()
+    if args.only != "":
+        starters = {KRS(args.only)}
+
+    children = graph.all_descendants(starters)
+    pprint(children)
+    children_companies = set(
+        company for krs, company in graph.companies.items() if krs in children
+    )
+    to_scrape = (starters | children) - already_scraped
 
     print(f"Already scraped: {already_scraped}")
     print(f"To scrape: {to_scrape}")
-    print(f"Will cost: {len(to_scrape) * 0.15} PLN")
+    print("To scrape (children):")
+    to_scrape_children = set(filter(lambda x: x.krs in to_scrape, children_companies))
+    pprint(to_scrape_children)
+    parent_count = Counter(map(lambda x: x.parent, to_scrape_children))
+    pprint(parent_count.most_common(100))
+    print(f"Will cost: {len(to_scrape) * 0.10} PLN")
+    input("Press enter to continue...")
+    rejestr = Rejestr()
 
     for url in save_org_connections(to_scrape):
-        result = get_rejestr_io(url)
+        result = rejestr.get_rejestr_io(url)
+        sleep(0.3)
 
         if result is None:
             print(f"Skipping {url}")
