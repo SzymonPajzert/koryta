16b7d3c523fab907ceadb5efa3b35263472344d9
Szymon Pajzert <szymonpajzert@gmail.com>
2025-12-11 16:27:27 +0100

    Lazy initialization of FirestoreIO

--- data/scrapers/src/stores/firestore.py
+++ data/scrapers/src/stores/firestore.py
@@ -2,34 +2,31 @@ import sys
 from typing import Any, Iterable
 
 from google.cloud import firestore
+from memoized_property import memoized_property  # type:ignore
 
 
 class FirestoreIO:
-    db: firestore.Client
-
-    def __init__(self):
-        print("Connecting to Firestore...")
-        # Use a service account for authentication.
-        # Make sure to point to your service account key file.
-        # You can also use `gcloud auth application-default login`
+    @memoized_property
+    def db_client(self) -> firestore.Client:
+        print("Connecting to Firestore client...")
         try:
-            self.db = firestore.Client(project="koryta-pl", database="koryta-pl")
+            result = firestore.Client(project="koryta-pl", database="koryta-pl")
             print("Successfully connected to Firestore.")
+            return result
         except Exception as e:
             print(f"Failed to connect to Firestore: {e}")
             print("Please ensure you have authenticated correctly.")
+            print("Use a service account for authentication.")
+            print("Make sure to point to your service account key file.")
+            print("You can also use `gcloud auth application-default login`")
             sys.exit(1)
 
-    def read_collection(
-        self, collection: str, stream=True, filters: list[tuple[str, str, Any]] = []
-    ) -> Iterable:
+    def read_collection(self, collection: str, stream=True, filters: list[tuple[str, str, Any]] = []) -> Iterable:
         print(f"Reading collection {collection} with {filters}")
 
-        collection_ref = self.db.collection(collection)
+        collection_ref = self.db_client.collection(collection)
         for field, op, value in filters:
-            collection_ref = collection_ref.where(
-                filter=firestore.FieldFilter(field, op, value)
-            )
+            collection_ref = collection_ref.where(filter=firestore.FieldFilter(field, op, value))
         if stream:
             for elt in collection_ref.stream():
                 yield elt
