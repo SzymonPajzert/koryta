7a43d116bee8b9cd0652da6a017fffd401ed2369
ROMB <szymonpajzert@gmail.com>
2025-12-16 12:04:56 +0100

    Restore UI e2e tests and remove top-level cypress dir (#224)
    * Remove obsolete cypress dir
    * UI - Restore some e2e tests

--- cypress/e2e/list.cy.js
+++
@@ -1,9 +0,0 @@
-describe("list", () => {
-  beforeEach(() => {
-    cy.visit("/lista");
-  });
-
-  it("screenshots", () => {
-    cy.wait(1500).get("body").matchImageSnapshot();
-  });
-});
--- cypress/e2e/pomoc.cy.js
+++
@@ -1,9 +0,0 @@
-describe("list", () => {
-  beforeEach(() => {
-    cy.visit("/pomoc");
-  });
-
-  it("screenshots", () => {
-    cy.wait(1500).get("body").matchImageSnapshot();
-  });
-});
