7f2313efb9cbfb1c058d4880c09ad965a8175848
Szymon Pajzert <szymonpajzert@gmail.com>
2025-12-20 14:12:49 +0100

    Small fixes

--- frontend/app/plugins/firebase-emulator.client.ts
+++ frontend/app/plugins/firebase-emulator.client.ts
@@ -24,9 +24,11 @@ export default defineNuxtPlugin((nuxtApp) => {
 
     // Firestore
     const db = getFirestore(app);
+    const namedDb = getFirestore(app, "koryta-pl");
     try {
         connectFirestoreEmulator(db, "127.0.0.1", 8080);
-        console.log("✅ Firestore Emulator connected to 127.0.0.1:8080");
+        connectFirestoreEmulator(namedDb, "127.0.0.1", 8080);
+        console.log("✅ Firestore Emulator connected to 127.0.0.1:8080 (default & koryta-pl)");
     } catch(e) { console.log("Firestore emulator already connected/failed", e); }
 
     // Functions
