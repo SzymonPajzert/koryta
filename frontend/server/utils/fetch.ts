0bdc2ecb10b9b944e6e646e5c326157a1971218f
Szymon Pajzert <szymonpajzert@gmail.com>
2026-01-04 19:18:07 +0100

    Remove unused fetch filters

--- frontend/server/utils/fetch.ts
+++ frontend/server/utils/fetch.ts
@@ -16,37 +16,20 @@ interface nodeData {
   record: unknown;
 }
 
-export interface NodeFilters {
-  interesting?: boolean;
-  posted?: boolean;
-  article?: boolean;
-}
-
 export interface FetchNodesOptions {
   isAuth?: boolean;
   nodeId?: string;
-  filters?: NodeFilters;
 }
 
 export async function fetchNodes<N extends NodeType>(
   path: N,
   options: FetchNodesOptions = {},
 ): Promise<Record<string, nodeData[N]>> {
-  const { isAuth = false, nodeId, filters } = options;
+  const { isAuth = false, nodeId } = options;
   const db = getFirestore("koryta-pl");
-  let query: FirebaseFirestore.Query = db
+  const query: FirebaseFirestore.Query = db
     .collection("nodes")
     .where("type", "==", path);
-
-  if (filters?.interesting) {
-    query = query.where("interesting", "==", true);
-  }
-  if (filters?.posted) {
-    query = query.where("posted", "==", true);
-  }
-  if (filters?.article) {
-    query = query.where("article", "==", true);
-  }
 
   if (nodeId) {
     const docRef = db.collection("nodes").doc(nodeId);
