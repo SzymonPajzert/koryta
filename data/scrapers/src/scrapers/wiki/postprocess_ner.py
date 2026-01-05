e440cfbbcfc9d59cfdf2db35d176b2896706e917
Mikołaj Pacek <miki.pacman@gmail.com>
2026-01-05 17:48:03 +0100

    Make sure that all names from text can be found in text.
    Also scrapy script for testing ner with Gemini.

--- data/scrapers/src/scrapers/wiki/postprocess_ner.py
+++ data/scrapers/src/scrapers/wiki/postprocess_ner.py
@@ -1,12 +1,13 @@
+import os
+
 import pandas as pd
 from google import genai
 from joblib import Memory
 from tqdm import tqdm
-import os
 
 prompt = "Given a wikipedia article please list all people mentioned in this article in a list and output as in python. Example output: [Ala Biba, Barack Obama]\n\n"
 memory = Memory(location="/tmp/genai_cache", verbose=0)
-client = genai.Client(api_key=os.environ["GENAI_API_KEY"])
+client = genai.Client(api_key=os.environ["GENAI_API_KEY"])  # get one from https://aistudio.google.com/
 
 
 def iou(a: list[str], b: list[str]) -> float:
@@ -23,7 +24,7 @@ def parse_response(response: str) -> list[str]:
 
 @memory.cache
 def get_response(prompt: str) -> str:
-    pred = client.models.generate_content(model="gemini-3-flash-preview", contents=prompt)
+    pred = client.models.generate_content(model="gemini-2.5-flash-lite", contents=prompt)
     return pred
 
 
@@ -43,31 +44,48 @@ def words_not_in_text(names: list[str], text: str) -> list[str]:
     return words
 
 
+def recall(pred_names: list[str], names_in_text: list[str], names_normalized: list[str]) -> float:
+    assert len(names_in_text) == len(names_normalized)
+    pred_names = set(n.lower() for n in pred_names)
+    matched = 0
+    for nt, nn in zip(names_in_text, names_normalized):
+        if nt.lower() in pred_names or nn.lower() in pred_names:
+            matched += 1
+    return matched / len(names_in_text)
+
+
 def main():
     df = pd.read_json("versioned/person_wikipedia_ner/person_wikipedia_ner.jsonl", lines=True)
     df["n_names"] = df["names"].apply(len)
-    ex_df = df[df["n_names"].le(100) & df["n_names"].ge(10)].sample(10, random_state=42)
-    print(ex_df)
+    df["text_len"] = df["text"].apply(len)
+    df["has_polityk"] = df["text"].apply(lambda x: "polityk" in x.lower())
+    df["names_in_text"] = df["names"].apply(lambda x: [name["name_in_text"] for name in x])
+    df["names_normalized"] = df["names"].apply(lambda x: [name["name_normalize"] for name in x])
+    ex_df = df[df["n_names"].le(30) & df["n_names"].ge(3) & df["text_len"].le(10_000)].sample(100, random_state=42)
 
     preds = []
     for i, row in tqdm(ex_df.iterrows(), total=len(ex_df), desc="Processing examples"):
-        pred = get_response(prompt + row["text"])
-
         try:
-            pred_names = parse_response(pred.text)
+            pred = get_response(prompt + row["text"])
+            pred_names = sorted(parse_response(pred.text))
         except Exception as e:
             pred_names = None
 
-        preds.append(sorted(pred_names))
+        preds.append(pred_names)
 
     ex_df["pred_names"] = preds
-    ex_df["percent_names_in_text"] = ex_df.apply(lambda r: percent_words_in_text(r["names"], r["text"]), axis=1)
+    ex_df = ex_df[~ex_df["pred_names"].isna()]
     ex_df["percent_pred_names_in_text"] = ex_df.apply(lambda r: percent_words_in_text(r["pred_names"], r["text"]),
                                                       axis=1)
-    ex_df["iou"] = ex_df.apply(lambda r: iou(r["names"], r["pred_names"]), axis=1)
-    ex_df["names_not_in_text"] = ex_df.apply(lambda r: words_not_in_text(r["names"], r["text"]), axis=1)
+    ex_df["iou"] = ex_df.apply(lambda r: iou(r["names_in_text"], r["pred_names"]), axis=1)
+    ex_df["names_not_in_text"] = ex_df.apply(lambda r: words_not_in_text(r["names_in_text"], r["text"]), axis=1)
     ex_df["pred_names_not_in_text"] = ex_df.apply(lambda r: words_not_in_text(r["pred_names"], r["text"]), axis=1)
+    ex_df["recall"] = ex_df.apply(
+        lambda r: recall(r["pred_names"], r["names_in_text"], r["names_normalized"]), axis=1
+    )
 
+    print(ex_df["recall"].mean())
+    print(ex_df.shape)
     import IPython
     IPython.embed()
 
