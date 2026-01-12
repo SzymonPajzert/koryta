94bacaaa926b201dbfe57ce0add40fffb77ac62a
upturnow <upturnow@proton.me>
2026-01-12 23:15:16 +0100

    load only chosen NER model

--- data/scrapers/src/stores/nlp.py
+++ data/scrapers/src/stores/nlp.py
@@ -4,7 +4,13 @@ from stores.textmodel.ner import HerbertNERClient, StanzaNERClient
 
 
 class NLPImpl(NLP):
-    def __init__(self):
-        self.herbert_ner_client = HerbertNERClient()
-        self.stanza_ner_client = StanzaNERClient()
+    def __init__(self, use_herbert_ner: bool = False, use_stanza_ner: bool = True):
+        self.herbert_ner_client: HerbertNERClient | None = None
+        self.stanza_ner_client: StanzaNERClient | None = None
+
+        if use_herbert_ner:
+            self.herbert_ner_client = HerbertNERClient()
+        if use_stanza_ner:
+            self.stanza_ner_client = StanzaNERClient()
+
 
