1e3d17c133205a3446b092c4e3672639e272e7ac
Mikołaj Pacek <miki.pacman@gmail.com>
2026-02-06 11:18:31 +0100

    New tests

--- data/scrapers/src/scrapers/article/parse_test.py
+++ data/scrapers/src/scrapers/article/parse_test.py
@@ -3,7 +3,7 @@ import requests
 
 from . import parse
 
-URL_DF_PATH = "/home/mp/Projects/koryta/data/scrapers/first_10.csv"
+URL_DF_PATH = "/home/mp/Downloads/all.csv"
 HEADERS = {"User-Agent": "KorytaCrawler/0.1 (+http://koryta.pl/crawler)"}
 
 
@@ -15,7 +15,11 @@ def fetch_url(url: str) -> bytes:
 def pytest_generate_tests(metafunc):
     """Dynamically generates test cases from the CSV file."""
     df = pd.read_csv(URL_DF_PATH)
+    assert set(df.columns) == {"Link", "Artykuł", "Domena", "Tytuł", "Tytuł Alternatywny", "Data", "Data Normalizowana",
+                               "Kto", "Gdzie"}
+    df["Data Normalizowana"] = df["Data Normalizowana"].replace("Brak", None)
     df["Data Normalizowana"] = pd.to_datetime(df["Data Normalizowana"]).dt.date
+    df["Tytuł Alternatywny"] = df["Tytuł Alternatywny"].replace("", None)
 
     if "row_article" in metafunc.fixturenames:
         # Filter for rows that ARE articles
@@ -31,14 +35,24 @@ def pytest_generate_tests(metafunc):
 def test_article(row_article):
     url = row_article["Link"]
     expected_title = row_article["Tytuł"]
+    alternative_title = row_article["Tytuł Alternatywny"]
     expected_date = row_article["Data Normalizowana"]
 
     website_bytes = fetch_url(url)
     parsed_data = parse.extract_article_content(website_bytes)
 
     assert parsed_data["is_article"], f"Failed for {url}: Should be an article"
-    assert parsed_data["title"] == expected_title
-    assert parsed_data["publication_date"] == expected_date, f"Hint, the date in article looks like '{row_article['Data']}'"
+
+    if not pd.isna(alternative_title):
+        assert parsed_data["title"] in (expected_title, alternative_title)
+    else:
+        assert parsed_data["title"].strip() == expected_title.strip()
+
+    if not pd.isna(expected_date):
+        assert parsed_data["publication_date"] == expected_date, \
+            f"Hint, the date in the article looks like '{row_article['Data']}'"
+    else:
+        assert parsed_data["publication_date"] is None
 
 
 def test_no_article(row_no_article):
