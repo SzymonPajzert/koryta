914f9490adb6cd7f842900ddd08040fbdd010bcd
Szymon Pajzert <szymonpajzert@gmail.com>
2026-02-28 11:49:09 +0100

    Move graph logic to useGraph composable

--- frontend/app/pages/graf.vue
+++ frontend/app/pages/graf.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <GraphCanvas />
+  <GraphCanvas :filtered="filtered" />
 </template>
 
 <script setup lang="ts">
+const { filtered } = useParams("Graf ");
+
 definePageMeta({
   title: "Graf",
   isGraph: true,
