1a42818a0d8b715c197e7ed44e27a82e2541b695
Szymon Pajzert <szymonpajzert@gmail.com>
2026-03-12 17:52:03 +0100

    Start checking for untyped funcs in quick-check

--- data/scrapers/quick_check.sh
+++ data/scrapers/quick_check.sh
@@ -3,7 +3,8 @@ set -e
 poetry run ruff check --fix
 poetry run ruff check
 poetry run lint-imports
-poetry run mypy src
+poetry run mypy src 
+poetry run mypy src/analysis/interesting.py --check-untyped-defs 
 poetry run pytest --ignore src/tests
 
 echo "I'll run all the tests now"
