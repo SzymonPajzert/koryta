ddaf1a76c53cee58d6add1fa23e00343d850d31c
Mikołaj Pacek <miki.pacman@gmail.com>
2026-03-17 18:15:37 +0100

    remove some flags from cli

--- data/scrapers/src/crawl_cli.py
+++ data/scrapers/src/crawl_cli.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import argparse
+import logging
 from argparse import ArgumentParser
 from pathlib import Path
 from uuid import uuid4
@@ -21,15 +22,16 @@ def _build_parser() -> ArgumentParser:
     parser.add_argument("--per-url-max-retries", type=int, default=3)
     parser.add_argument("--lock-timeout-seconds", type=int, default=60)
     parser.add_argument("--per-domain-rate-limit-qpm", type=int, default=20)
-    parser.add_argument("--url-scoring-function", choices=["default", "kalisz"], default="default")    # TODO hook registered function here
+    parser.add_argument("--url-scoring-function", choices=["default", "kalisz"],
+                        default="default")  # TODO hook registered function here
     return parser
 
 
-def _build_options(args: argparse.Namespace) -> CrawlOptions:  # type: ignore
+def _build_options(args: argparse.Namespace) -> CrawlOptions:
     return CrawlOptions(
         worker_id=args.worker_id,
         storage_type=args.storage_type,
-        local_output=args.local_output if args.storage == "local" else None,
+        local_output=args.local_output if args.storage_type == "local" else None,
         per_url_max_retries=args.per_url_max_retries,
         lock_timeout_seconds=args.lock_timeout_seconds,
         per_domain_rate_limit_seconds=args.per_domain_rate_limit_qpm,
@@ -37,14 +39,27 @@ def _build_options(args: argparse.Namespace) -> CrawlOptions:  # type: ignore
     )
 
 
+def _setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+        handlers=[
+            logging.StreamHandler()
+        ]
+    )
+
+
 def main() -> None:
+    _setup_logging()
     parser = _build_parser()
     args = parser.parse_args()
     options = _build_options(args)
+    logging.info("Running crawler with options: %s", options)
 
     queue = PostgresCrawlQueue.from_env()
-    ctx, _ = _setup_context(False, crawl_queue=queue)
+    logging.info("Initializing crawling queue")
 
+    ctx, _ = _setup_context(False, crawl_queue=queue)
     run_crawler(ctx, options)
 
 
