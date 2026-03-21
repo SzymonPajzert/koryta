c1790af9ed3691750cec64fa2a81559e5f94779f
Szymon Pajzert <szymonpajzert@gmail.com>
2026-03-21 15:42:19 +0100

    Add validation to /api/nodes/[id] and fix returning wrong revision data

--- frontend/server/api/nodes/[id].get.ts
+++ frontend/server/api/nodes/[id].get.ts
@@ -1,4 +1,4 @@
-import { getFirestore } from "firebase-admin/firestore";
+import { getFirestore, type DocumentReference } from "firebase-admin/firestore";
 import { getApp } from "firebase-admin/app";
 import { pageIsPublic } from "~~/shared/model";
 import { authCachedEventHandler } from "~~/server/utils/handlers";
@@ -6,12 +6,13 @@ import { z } from "zod";
 import type { Node } from "~~/shared/model";
 
 const queryValidator = z.object({
-  latest: z.boolean().optional(),
+  latest: z.string().optional(),
 });
 
 const responseValidator = z.object({
   name: z.string(),
   type: z.enum(["person", "place", "article", "region"]),
+  revision_id: z.string().optional(),
 });
 
 export default authCachedEventHandler(async (event) => {
@@ -24,7 +25,7 @@ export default authCachedEventHandler(async (event) => {
   );
 
   const db = getFirestore(getApp(), "koryta-pl");
-  const node = await (query.latest
+  const node = await (query.latest == "true"
     ? getLatestRevision(db, id)
     : getEntity(db, id));
 
@@ -41,20 +42,33 @@ export default authCachedEventHandler(async (event) => {
 });
 
 async function getLatestRevision(db: FirebaseFirestore.Firestore, id: string) {
-  return (
+  const result = (
     await db
       .collection("revisions")
       .where("node_id", "==", id)
       .orderBy("update_time", "desc")
       .limit(1)
       .get()
-  ).docs.map((doc) => doc.data())[0];
+  ).docs.map((doc) => doc.data().data)[0];
+  // TODO get rid of this, each node should have a revision
+  if (!result) {
+    return await getEntity(db, id);
+  }
+  return result;
 }
 
 async function getEntity(db: FirebaseFirestore.Firestore, id: string) {
   const nodeDoc = await db.collection("nodes").doc(id).get();
-  if (nodeDoc.exists) {
-    return { id: nodeDoc.id, ...nodeDoc.data() };
+  if (!nodeDoc.exists) {
+    return undefined;
   }
-  return null;
+  const result: Record<string, unknown> = {
+    id: nodeDoc.id,
+    ...nodeDoc.data(),
+  };
+  if (result["revision_id"]) {
+    const refId: DocumentReference = result["revision_id"] as any;
+    result["revision_id"] = refId.path;
+  }
+  return result;
 }
