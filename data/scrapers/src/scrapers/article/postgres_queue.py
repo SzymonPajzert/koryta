48b50e6fc6923265712fedd3cd158811a4a7ba35
Mikołaj Pacek <miki.pacman@gmail.com>
2026-03-23 22:22:40 +0100

    Skip blocked

--- data/scrapers/src/scrapers/article/postgres_queue.py
+++ data/scrapers/src/scrapers/article/postgres_queue.py
@@ -201,11 +201,6 @@ class PostgresCrawlQueue(CrawlQueue):
                     WHERE wi.done = FALSE
                       AND wi.num_retries < %s
                       AND (wi.locked_by_worker_id IS NULL OR wi.locked_at <= %s)
-                      AND NOT EXISTS (
-                          SELECT 1 FROM blocked_domains bd
-                          WHERE wi.url = bd.domain
-                             OR wi.url LIKE bd.domain || '/%%'
-                      )
                     ORDER BY wi.priority ASC, wi.id ASC
                     LIMIT 1
                     FOR UPDATE SKIP LOCKED
