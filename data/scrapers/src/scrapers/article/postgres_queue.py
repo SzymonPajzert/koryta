07d70b65fa065fd2894b8ed555b64bdf31d2673b
Mikołaj Pacek <miki.pacman@gmail.com>
2026-03-25 17:13:20 +0100

    Bring back filtering blocked

--- data/scrapers/src/scrapers/article/postgres_queue.py
+++ data/scrapers/src/scrapers/article/postgres_queue.py
@@ -201,6 +201,11 @@ class PostgresCrawlQueue(CrawlQueue):
                     WHERE wi.done = FALSE
                       AND wi.num_retries < %s
                       AND (wi.locked_by_worker_id IS NULL OR wi.locked_at <= %s)
+                      AND NOT EXISTS (
+                          SELECT 1 FROM blocked_domains bd
+                          WHERE wi.url = bd.domain
+                             OR wi.url LIKE bd.domain || '/%%'
+                      )
                     ORDER BY wi.priority ASC
                     LIMIT 1
                     FOR UPDATE SKIP LOCKED
