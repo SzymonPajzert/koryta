abe8d4adecb55a11418933f72792265c65a89eeb
Mikołaj Pacek <miki.pacman@gmail.com>
2026-03-25 18:24:36 +0100

    Better printing url

--- data/scrapers/src/entities/util.py
+++ data/scrapers/src/entities/util.py
@@ -65,5 +65,5 @@ class NormalizedParse:
             hostname=hostname,
             hostname_normalized=hostname_normalized,
             domain=domain,
-            full_url=domain + parsed.path
+            full_url=domain + parsed.path + ("?" + parsed.query if parsed.query != "" else ""),
         )
