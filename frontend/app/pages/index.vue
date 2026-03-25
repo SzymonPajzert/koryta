5df9ceb7fab5b82f98270daeb63010da728232db
Szymon Pajzert <szymonpajzert@gmail.com>
2026-03-25 23:06:56 +0100

    Remove links to graph pages

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -66,41 +66,6 @@
       <CardCallToAction />
     </v-col>
 
-    <v-col cols="12" md="6">
-      <v-card
-        to="/graf?miejsce=0qGH8AL9bwUCQvrLNwiO"
-        class="py-4"
-        color="surface-variant"
-        variant="tonal"
-        rounded="lg"
-      >
-        <v-card-title>
-          <h2 class="text-h5 font-weight-bold">Zobacz jak PSL przejęło PKP</h2>
-        </v-card-title>
-        <v-card-text>
-          <v-img class="mb-4" height="250" src="@/assets/psl_pkp.png" />
-        </v-card-text>
-      </v-card>
-    </v-col>
-    <v-col cols="12" md="6">
-      <v-card
-        to="/graf?miejsce=YLMeu6hJgYGUokxec9D8"
-        class="py-4"
-        color="surface-variant"
-        variant="tonal"
-        rounded="lg"
-      >
-        <v-card-title>
-          <h2 class="text-h6 font-weight-bold">
-            Albo PL2050 ministerstwo klimatu
-          </h2>
-        </v-card-title>
-        <v-card-text>
-          <v-img class="mb-4" height="250" src="@/assets/polska2050.png" />
-        </v-card-text>
-      </v-card>
-    </v-col>
-
     <CardHomeItem router="/pomoc" icon="mdi-plus-box-outline">
       <template #header> Dodaj osoby i artykuły </template>
       Dodaj brakujące osoby w spółkach państwa lub samorządu albo linki do
