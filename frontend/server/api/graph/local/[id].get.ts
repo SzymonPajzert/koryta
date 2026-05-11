a0fce0981e5c2af31ec9e79ba49b5227a30cc51f
Szymon Pajzert <szymonpajzert@gmail.com>
2026-05-11 14:28:13 +0200

    Re-enable /api/graph/local accepting latest argument

--- frontend/server/api/graph/local/[id].get.ts
+++ frontend/server/api/graph/local/[id].get.ts
@@ -107,7 +107,7 @@ export async function getLocalGraph(
 
 export default authCachedEventHandler(async (event) => {
   const query = getQuery(event);
-  const _latest = query.latest !== undefined && query.latest !== "false";
+  const latest = query.latest !== undefined && query.latest !== "false";
   const distance = query.distance ? parseInt(query.distance as string, 10) : 1;
   const focusNodeId = getRouterParam(event, "id");
 
@@ -121,5 +121,5 @@ export default authCachedEventHandler(async (event) => {
   }
 
   // TODO actually propagate the information about the latest
-  return getLocalGraph(focusNodeId, false, distance, expansions);
+  return getLocalGraph(focusNodeId, latest, distance, expansions);
 });
