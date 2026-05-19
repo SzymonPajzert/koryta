a96c2f704f64f1458597b8e3745d68af44dd2974
Szymon Pajzert <szymonpajzert@gmail.com>
2026-05-19 19:13:23 +0200

    Small utility script to submit people to the frontend

---
+++ data/scrapers/submit_people.sh
@@ -0,0 +1,15 @@
+set -e
+
+TERYT=$1
+if [[ $2 == prod ]]; then
+	SUFFIX="--prod --endpoint https://autopush.koryta.pl"
+fi
+
+echo $TERYT
+echo $SUFFIX
+
+
+echo "koryta PeoplePayloads --region $TERYT --ignore-elections --currently-employed | koryta_uploader --type person --submit $SUFFIX" 
+koryta PeoplePayloads --region $TERYT --ignore-elections --currently-employed --output stderr 2>&1 1>/dev/null | koryta_uploader --type person --submit $SUFFIX 
+echo "koryta PeoplePayloads --region $TERYT | koryta_uploader --type person --submit $SUFFIX"
+koryta PeoplePayloads --region $TERYT --output stderr 2>&1 1>/dev/null | koryta_uploader --type person --submit $SUFFIX 
