9ce251dae94789b6cf99353a4714cfa9daf19355
Szymon Pajzert <szymonpajzert@gmail.com>
2026-05-20 10:06:23 +0200

    Add /eksploruj/statystyki page showing stats of the exploration

---
+++ frontend/server/api/stats/votes.get.ts
@@ -0,0 +1,21 @@
+import { getFirestore } from "firebase-admin/firestore";
+import { getApp } from "firebase-admin/app";
+import { logEventPath } from "~~/server/utils/fetch";
+import type { VoteDocument } from "~~/shared/model";
+
+export default defineEventHandler(async (_event) => {
+  const db = getFirestore(getApp(), "koryta-pl");
+
+  const votesSnapshot = await db.collection("votes").get();
+  const votes = votesSnapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as (VoteDocument & { id: string })[];
+
+  logEventPath("fetchVotes", "all", {
+    collection: "votes",
+    size: votes.length,
+  });
+
+  return votes;
+});
