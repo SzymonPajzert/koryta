b4ccf26285b6fcfde4da58cab2f9c4e08e5983db
Szymon Pajzert <szymonpajzert@gmail.com>
2026-05-25 15:09:42 +0200

    listing people doesn't run on the server if requesting private

--- frontend/app/composables/entity/listWithStats.ts
+++ frontend/app/composables/entity/listWithStats.ts
@@ -6,12 +6,13 @@ import { useCurrentUser, useIsCurrentUserLoaded } from "vuefire";
 
 export async function useListWithStats(
   apiQuery: Ref<Query> | ComputedRef<Query>,
+  cacheKey: string = "tabela-data",
 ) {
   const user = useCurrentUser();
   const isAuthReady = useIsCurrentUserLoaded();
 
   const { data: pageData, pending } = await useAsyncData(
-    "tabela-data",
+    cacheKey,
     async () => {
       if (!isAuthReady.value) {
         await new Promise<void>((resolve) => {
@@ -74,6 +75,7 @@ export async function useListWithStats(
     },
     {
       watch: [apiQuery, user],
+      server: apiQuery.value.visibility !== "private",
     },
   );
 
