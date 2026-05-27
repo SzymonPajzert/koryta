4682781a13ad261620a0a5ea07c389ce43777be9
Szymon Pajzert <szymonpajzert@gmail.com>
2026-05-27 15:45:28 +0200

    Check that we scrape both aktualne and historyczne for krs entries

---
+++ data/scrapers/src/tests/pipelines/test_krs_already_scraped.py
@@ -0,0 +1,26 @@
+import pytest
+
+from koryta import setup_context
+from pipelines import KRSAlreadyScraped
+
+
+@pytest.fixture(scope="module")
+def ctx():
+    return setup_context(False)[0]
+
+
+def test_both_aktualnosc_are_scraped(ctx):
+    df = KRSAlreadyScraped().read_or_process(ctx)
+    per_method = df[["krs", "method"]].groupby("krs").aggregate(lambda d: d.unique())
+    print(per_method.head())
+    print(per_method.loc["0000384025"])
+
+    only_one_aktualnosc = per_method["method"].apply(
+        lambda methods: (
+            ("aktualnosc_aktualne" in methods) ^ ("aktualnosc_historyczne" in methods)
+        )
+    )
+    only_one_aktualnosc = per_method[only_one_aktualnosc]
+    print(only_one_aktualnosc.head())
+
+    assert len(only_one_aktualnosc) == 0
