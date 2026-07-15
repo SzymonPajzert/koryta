a73b54cecfc3ae73670f54565b3b537220be0559
Szymon Pajzert <szymonpajzert@gmail.com>
2026-07-15 15:54:32 +0200

    /api/revisions/create POST method to create edit suggestions

---
+++ frontend/server/api/revisions/create.post.ts
@@ -0,0 +1,41 @@
+import { getFirestore, Timestamp } from "firebase-admin/firestore";
+import { getApp } from "firebase-admin/app";
+import { getUser } from "~~/server/utils/auth";
+import { sanitizeFirestoreData } from "~~/server/utils/revisions";
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+
+  if (!body.node_id) {
+    throw createError({
+      statusCode: 400,
+      message: "Missing required node_id",
+    });
+  }
+
+  const user = await getUser(event);
+  if (!user) {
+    throw createError({
+      statusCode: 401,
+      message: "Unauthorized",
+    });
+  }
+
+  const db = getFirestore(getApp(), "koryta-pl");
+  const revisionRef = db.collection("revisions").doc();
+  const timestamp = Timestamp.now();
+  const { node_id, ...dataFields } = body;
+
+  const revision = {
+    node_id: node_id,
+    data: sanitizeFirestoreData(dataFields),
+    update_time: timestamp,
+    update_user: user.uid,
+    update_automatic: false,
+    status: "pending",
+  };
+
+  await revisionRef.set(revision);
+
+  return { id: revisionRef.id };
+});
