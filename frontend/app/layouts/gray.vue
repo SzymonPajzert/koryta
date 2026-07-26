76696bcbc96ee6ad67f092e1364f643e43b498f4
ROMB <szymonpajzert@gmail.com>
2026-07-26 17:29:43 +0000

    Create gray layout

---
+++ frontend/app/layouts/gray.vue
@@ -0,0 +1,18 @@
+<template>
+  <NuxtLayout name="default">
+    <!-- Pages using this layout must also set fullWidth: true in their page
+         meta, otherwise the default layout constrains and pads the grey band. -->
+    <div class="w-100 bg-grey-lighten-4">
+      <v-container fluid class="pa-4 py-md-10">
+        <v-sheet
+          class="mx-auto pa-6 pa-md-10"
+          max-width="1200"
+          rounded="lg"
+          border
+        >
+          <slot />
+        </v-sheet>
+      </v-container>
+    </div>
+  </NuxtLayout>
+</template>
